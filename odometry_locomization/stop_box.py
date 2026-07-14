e5e7180dc6fe91dbfaa9dea4d278dd52b3752b07
TejasLamba2006 <tejas22feb@gmail.com>
2026-07-14 19:40:18 +0530

    feat: Add asyncio import and sys.path configuration in stop_box.py

--- odometry_locomization/stop_box.py
+++ odometry_locomization/stop_box.py
@@ -1,5 +1,6 @@
 # stop_box.py -- stop MKBOXPRO streaming
-import asyncio
+import asyncio, sys
+sys.path.insert(0, '/usr/tejas/x-linux-rbt1/odometry_locomization')
 from imu_usb_mkbox import MkBoxUsbGyro
 
 async def main():
